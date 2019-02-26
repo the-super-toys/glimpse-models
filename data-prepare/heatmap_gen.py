321116d59644ab1460c87bd8e7b4659785d75465
Victor Albertos <victor-albertos@cookpad.com>
2019-02-26 18:18:18 +0100

    refactor hetmap generation

--- data-prepare/heatmap_gen.py
+++ data-prepare/heatmap_gen.py
@@ -1,41 +1,33 @@
 from PythonAPI.salicon.salicon import SALICON
 import scipy.misc
 
-# initialize salicon dataset
+# The directory has to exist before running this task
+output_heatmaps = '../dataset/heatmaps/'
+input_fixations_train = 'annotations/fixations_train2014.json'
+input_fixations_val = 'annotations/fixations_val2014.json'
+
+
+"""Supplying the path to either the train or val annotations generates their associated headmap
+"""
+def generate_heatmap(path):
+    salicon = SALICON(path)
+
+    print("getting image IDs")
+    imgIds = salicon.getImgIds()
+
+    for imgId in imgIds:
+        img = salicon.loadImgs(imgId)[0]
+        annIds = salicon.getAnnIds(imgIds=img['id'])
+        anns = salicon.loadAnns(annIds)
+        heatmap = salicon.buildFixMap(anns)
+
+        print("saving heatmap for " + img['file_name'] + " " + str(heatmap.shape))
+
+        scipy.misc.imsave(output_heatmaps + img['file_name'], heatmap)
+
+
 print('train')
-salicon = SALICON("../annotations/fixations_train2014.json")
-
-print("getting image IDs")
-imgIds = salicon.getImgIds()
-
-for imgId in imgIds:
-    img = salicon.loadImgs(imgId)[0]
-    annIds = salicon.getAnnIds(imgIds=img['id'])
-    anns = salicon.loadAnns(annIds)
-    heatmap = salicon.buildFixMap(anns)
-
-    print("saving heatmap for " + img['file_name'] + " " + str(heatmap.shape))
-
-    scipy.misc.imsave('../heatmaps/' + img['file_name'], heatmap)
-
-    # df = pd.DataFrame(heatmap)
-    # df.to_csv('../heatmaps/' + img['file_name'].replace(".jpg", ".csv"), header=None, index=None)
+generate_heatmap(input_fixations_train)
 
 print('valid')
-salicon = SALICON("../annotations/fixations_val2014.json")
-
-print("getting image IDs")
-imgIds = salicon.getImgIds()
-
-for imgId in imgIds:
-    img = salicon.loadImgs(imgId)[0]
-    annIds = salicon.getAnnIds(imgIds=img['id'])
-    anns = salicon.loadAnns(annIds)
-    heatmap = salicon.buildFixMap(anns)
-
-    print("saving heatmap for " + img['file_name'] + " " + str(heatmap.shape))
-
-    scipy.misc.imsave('../heatmaps/' + img['file_name'], heatmap)
-
-    # df = pd.DataFrame(heatmap)
-    # df.to_csv('../heatmaps/' + img['file_name'].replace(".jpg", ".csv"), header=None, index=None)
+generate_heatmap(input_fixations_val)
